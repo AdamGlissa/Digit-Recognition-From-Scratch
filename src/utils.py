aedb1e0c5d01d76ceb65ac7ed34e93ee7eb86f36
AdamGlissa <adamglissa@gmail.com>
2025-11-06 11:36:46 +0100

    Correction du débordement numérique avec softmax

--- src/utils.py
+++ src/utils.py
@@ -8,4 +8,14 @@ def relu_derivative(z: np.ndarray) -> np.ndarray:
     return np.where(z > 0, 1, 0)
 
 def softmax(z: np.ndarray) -> np.ndarray:
-    return np.exp(z) / np.sum(np.exp(z))
+    res = 0
+    if z.ndim == 1:
+        e_z = np.exp(z - np.max(z))
+        res = e_z / e_z.sum()
+    else:
+        e_z = np.exp(z - np.max(z, azis=1, keepdims=True))
+        res = e_z / e_z.sum(azis=1, keepdims=True)
+        
+    return res
+
+print(softmax(np.array([1.0, 2.0, 3.0])))
